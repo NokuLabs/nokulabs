d006475550803a5fba470e122555a083f492a41b
NokuLabs <745jdth7d7@privaterelay.appleid.com>
2026-03-02 01:55:44 +0200

    chore: remove legacy metadata and tighten types/utils

--- src/types/index.ts
+++ src/types/index.ts
@@ -8,21 +8,3 @@ export interface ServiceCard {
   description: string
   icon: string
 }
-
-export interface ApproachColumn {
-  title: string
-  description: string
-}
-
-export interface TrustPillar {
-  title: string
-  description: string
-  icon: string
-}
-
-export interface ContactFormData {
-  name: string
-  email: string
-  company: string
-  message: string
-}
